62777130500cfa91f6193c6e854d9126129109ff
Greg Morgan <gregm9999@gmail.com>
2019-01-19 20:31:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(blockRange) {
   return function isEnoughRange(start, end) {
     let startInt=parseInt(start, 10);
     let endInt=parseInt(end, 10);
-    let distance = Math.abs(startInt-endInt);
+    let travelDistance = Math.abs(startInt-endInt);
     return blockRange > distance ? `within range by ${distance-blockRange}` : `${blockRange-distance} blocks out of range`;
   }
 }
