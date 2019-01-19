54ca96430a7610636d4c0e4c2338e38bc4d062e8
Greg Morgan <gregm9999@gmail.com>
2019-01-19 20:37:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function produceTipCalculator(tipPercent) {
     return amount * tipPercent;
   }
 }
+
+function createDriver () {
+  let driverId=0;
+  class Driver {
+    constructor(name) {
+      this.name=name;
+      this.id=++driverId;
+    }
+  }
+}
